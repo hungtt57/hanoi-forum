0d085895a457844b5356151b11bf0de2e51ac385
Hin <hunguet1471994@gmail.com>
2018-03-19 17:06:14 +0700

    UPDATE

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public $fillable = ['content', 'title', 'user_id', 'status','meta_description','meta_keyword','meta_keyword','image'];
+    public $fillable = ['content', 'title', 'user_id', 'status','meta_description','meta_title','meta_keyword','image'];
     public $table = 'posts';
 }
--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -1,57 +1,51 @@
 <nav class="navbar navbar-white navbar-kawsa navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
-            <button aria-controls="navbar" aria-expanded="false" class="navbar-toggle collapsed" data-target="#navbar" data-toggle="collapse" type="button"><span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
-            <a class="navbar-brand" href="index.html"><img alt="" class="img-responsive" src="/frontend/assets/img/site-logo.png"></a>
+            <button aria-controls="navbar" aria-expanded="false" class="navbar-toggle collapsed" data-target="#navbar"
+                    data-toggle="collapse" type="button"><span class="sr-only">Toggle navigation</span> <span
+                        class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
+            <a class="navbar-brand" href="index.html"><img alt="" class="img-responsive"
+                                                           src="/frontend/assets/img/site-logo.png"></a>
         </div>
         <div class="navbar-collapse collapse sabbi-navbar-collapse  navbar-nav-hov_underline" id="navbar">
             <div class="nav-btn-wrap"><a href="#" class="btn btn-primary pull-right">Join Research</a></div>
             <ul class="nav navbar-nav navbar-right" id="menu-main-nav">
                 <li class="active">
-                    <a href="index-2.html" title="Home">Home</a>
+                    <a href="{{url('/')}}" title="Home">Home</a>
                 </li>
                 <li>
-                    <a href="lab_research.html" title="Research">Research</a>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" title="People"  data-hover="dropdown" data-animations="zoomIn">People <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <a href="professor.html" title="Dr Rushmore">Dr. Rushmore</a>
-                        </li>
-                        <li>
-                            <a href="ourteam.html" title="Our Team">Our Team</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown"  data-hover="dropdown" href="#" title="Publications" data-animations="zoomIn">Publications <span class="caret"></span></a>
-                    <ul class=" dropdown-menu" role="menu">
-                        <li>
-                            <a href="lab_book.html" title="Books">Books</a>
-                        </li>
-                        <li>
-                            <a href="lab_journal.html" title="Journal Articles">Journal Articles</a>
-                        </li>
-                        <li>
-                            <a href="lab_conf-talk.html" title="Conferences and Talks">Conferences and Talks</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown"  data-hover="dropdown" href="#" title="Events" data-animations="zoomIn">Events <span class="caret"></span></a>
-                    <ul class=" dropdown-menu" role="menu">
-                        <li>
-                            <a href="events.html" title="Books">Events Page</a>
-                        </li>
-                        <li>
-                            <a href="events_single.html" title="Journal Articles">Events Single Page</a>
-                        </li>
-                    </ul>
+                    <a href="{{url('/')}}" title="About">About</a>
                 </li>
                 <li>
-                    <a href="future_member.html" title="Future Member">Future Member</a>
+                    <a href="{{url('/')}}" title="Program">Program</a>
                 </li>
+                <li>
+                    <a href="{{url('/')}}" title="Publication">Publication</a>
+                </li>
+                <li>
+                    <a href="{{url('/')}}" title="News">News</a>
+                </li>
+                <li>
+                    <a href="{{url('/')}}" title="Multimedia">Multimedia</a>
+                </li>
+                <li>
+                    <a href="{{url('/')}}" title="FAQ">FAQ</a>
+                </li>
+                <li>
+                    <a href="{{url('/')}}" title="Contact Us">Contact Us</a>
+                </li>
+                {{--<li class="dropdown">--}}
+                {{--<a class="dropdown-toggle" data-toggle="dropdown" href="#" title="People"  data-hover="dropdown" data-animations="zoomIn">People <span class="caret"></span></a>--}}
+                {{--<ul class="dropdown-menu" role="menu">--}}
+                {{--<li>--}}
+                {{--<a href="professor.html" title="Dr Rushmore">Dr. Rushmore</a>--}}
+                {{--</li>--}}
+                {{--<li>--}}
+                {{--<a href="ourteam.html" title="Our Team">Our Team</a>--}}
+                {{--</li>--}}
+                {{--</ul>--}}
+                {{--</li>--}}
+
             </ul>
         </div>
     </div>
