3e09814e22d80ba5df0f8c49de93d76ff689fbe5
Hin <hunguet1471994@gmail.com>
2018-03-21 17:26:41 +0700

    up

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -48,11 +48,14 @@
             <div class="row home-info-block-first">
                 <div class="col-md-3 col-sm-6">
                     <article class="sabbi-thumlinepost-card solitude-bg__x">
-                        <figure class="sabbi-thumlinepost-card-figure">
-                            <img src="/frontend/assets/img/sabbi-post/card-1.png" alt="" class="img-responsive img-thumpost">
-                        </figure>
+                        <a href="{{url('climate-change-evidence-and-security')}}">
+                            <figure class="sabbi-thumlinepost-card-figure">
+                                <img src="/frontend/assets/img/sabbi-post/card-1.png" alt="" class="img-responsive img-thumpost">
+                            </figure>
+
                         <div class="sabbi-thumlinepost-card-meta">
-                            <h2 class="meta-title ht-5">Climate change evidence and security</h2>
+                            <a href="{{url('climate-change-evidence-and-security')}}" style="color:inherit">
+                            <h2 class="meta-title ht-5">Climate change evidence and security</h2></a>
                             <p class="meta-text"></p>
                             <a href="{{url('climate-change-evidence-and-security')}}" class="btn btn-unsolemn btn-action read-more">Read More</a>
                         </div>
@@ -60,11 +63,14 @@
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <article class="sabbi-thumlinepost-card solitude-bg__x">
+                        <a href="{{url('humans-impact-climate')}}" style="color:inherit">
                         <figure class="sabbi-thumlinepost-card-figure">
                             <img src="/frontend/assets/img/sabbi-post/card-2.png" alt="" class="img-responsive img-thumpost">
                         </figure>
+                        </a>
                         <div class="sabbi-thumlinepost-card-meta">
-                            <h2 class="meta-title ht-5">Humans impact climate</h2>
+                            <a href="{{url('humans-impact-climate')}}" style="color:inherit"> <h2 class="meta-title ht-5">Humans impact climate</h2> </a>
+
                             <p class="meta-text"></p>
                             <a href="{{url('humans-impact-climate')}}" class="btn btn-unsolemn btn-action read-more">Read More</a>
                         </div>
@@ -72,11 +78,15 @@
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <article class="sabbi-thumlinepost-card solitude-bg__x">
+                        <a href="{{url('climate-change-response')}}" style="color:inherit">
                         <figure class="sabbi-thumlinepost-card-figure">
                             <img src="/frontend/assets/img/sabbi-post/card-3.png" alt="" class="img-responsive img-thumpost">
                         </figure>
+                        </a>
                         <div class="sabbi-thumlinepost-card-meta">
+                            <a href="{{url('climate-change-response')}}" style="color:inherit">
                             <h2 class="meta-title ht-5">Climate change response</h2>
+                            </a>
                             <p class="meta-text"></p>
                             <a href="{{url('climate-change-response')}}" class="btn btn-unsolemn btn-action read-more">Read More</a>
                         </div>
@@ -84,11 +94,15 @@
                 </div>
                 <div class="col-md-3 col-sm-6">
                     <article class="sabbi-thumlinepost-card solitude-bg__x">
+                        <a href="{{url('policy-and-governance-of-climate-change-response-and-sustainability')}}" style="color:inherit">
                         <figure class="sabbi-thumlinepost-card-figure">
                             <img src="/frontend/assets/img/sabbi-post/card-4.png" alt="" class="img-responsive img-thumpost">
                         </figure>
+                        </a>
                         <div class="sabbi-thumlinepost-card-meta">
+                            <a href="{{url('policy-and-governance-of-climate-change-response-and-sustainability')}}" style="color:inherit">
                             <h2 class="meta-title ht-5">Policy and governance of climate change response and sustainability</h2>
+                            </a>
                             <p class="meta-text"></p>
                             <a href="{{url('policy-and-governance-of-climate-change-response-and-sustainability')}}" class="btn btn-unsolemn btn-action read-more">Read More</a>
                         </div>
@@ -99,11 +113,15 @@
             <div class="row home-info-block-first">
                 <div class="col-md-4 col-sm-6">
                     <article class="sabbi-thumlinepost-card solitude-bg__x">
+                        <a href="{{url('science-technology-and-education-for-climate-change-response-and-sustainability')}}" style="color:inherit">
                         <figure class="sabbi-thumlinepost-card-figure">
                             <img src="/frontend/assets/img/sabbi-post/card-1.png" alt="" class="img-responsive img-thumpost">
                         </figure>
+                        </a>
                         <div class="sabbi-thumlinepost-card-meta">
+                            <a href="{{url('science-technology-and-education-for-climate-change-response-and-sustainability')}}" style="color:inherit">
                             <h2 class="meta-title ht-5">Science, technology and education for climate change response and sustainability</h2>
+                            </a>
                             <p class="meta-text"></p>
                             <a href="{{url('science-technology-and-education-for-climate-change-response-and-sustainability')}}" class="btn btn-unsolemn btn-action read-more">Read More</a>
                         </div>
