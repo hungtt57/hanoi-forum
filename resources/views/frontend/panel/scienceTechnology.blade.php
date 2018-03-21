2f69d956d33f713db067edd83a7c90dac832a8cd
Hin <hunguet1471994@gmail.com>
2018-03-21 17:17:29 +0700

    up

--- resources/views/frontend/panel/scienceTechnology.blade.php
+++ resources/views/frontend/panel/scienceTechnology.blade.php
@@ -17,7 +17,7 @@
                             <meta charset="utf-8" />
                             <b id="docs-internal-guid-701c63ca-47a6-b698-73a5-58d58dc4b616">Moderator (TBC)</b></h2>
 
-                            <b dir="ltr"><b id="docs-internal-guid-701c63ca-47a6-b698-73a5-58d58dc4b616">Brief introduction: </b></b>
+                            <p dir="ltr"><b id="docs-internal-guid-701c63ca-47a6-b698-73a5-58d58dc4b616">Brief introduction: </b></p>
                             <p dir="ltr"><span id="docs-internal-guid-701c63ca-47bc-4b7c-25ee-80977d6a2b30">In this session, we will look at the relationship between education and training and the mission of response to climate change. In order to increase the effectiveness of climate change response in the long-term, it is essential to educate the public and next generations about this phenomenon. This raises the question of what and how climate change can be integrated into the current curriculum from the early stages to higher levels of education. Equally important, it is desired that higher education can well equip students with knowledge and skills to be able to apply science, technological progress and advancement to solve local, community-based problems. Thus, it is important to ask if the current institutional architecture in education is enable enough? If not, what can be done differently? </span></p>
 
                         </div>
