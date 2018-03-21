f1a6de43728597efa7e2025ab197a2bf2abc10ac
Hin <hunguet1471994@gmail.com>
2018-03-21 15:50:40 +0700

    UPDATE

--- resources/views/frontend/panel/policyAndGovernance.blade.php
+++ resources/views/frontend/panel/policyAndGovernance.blade.php
@@ -16,25 +16,25 @@
                         <div>
                             <h2><meta charset="utf-8" /></h2>
 
-                            <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">Climate change has wide-ranging impacts whose consequences burden poor developing countries most seriously. Dealing with this problem requires global cooperation. OECD analysis shows that large reductions in greenhouse gases emissions are achievable at relatively low costs, if the right policies are put in place. This includes strong use of market-based instruments world wide to develop a global price for GHG emissions, accompanied by better integration of climate change objectives in relevant policy areas such as energy, transport, building, agriculture or forestry, and other measures to speed technological innovation and diffusion.</b></p>
+                            <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">Climate change has wide-ranging impacts whose consequences burden poor developing countries most seriously. Dealing with this problem requires global cooperation. OECD analysis shows that large reductions in greenhouse gases emissions are achievable at relatively low costs, if the right policies are put in place. This includes strong use of market-based instruments world wide to develop a global price for GHG emissions, accompanied by better integration of climate change objectives in relevant policy areas such as energy, transport, building, agriculture or forestry, and other measures to speed technological innovation and diffusion.</span></p>
 
-                            <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">This session will try to discuss these issues, focusing on big questions as follows: </b></p>
+                            <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">This session will try to discuss these issues, focusing on big questions as follows: </span></p>
 
                             <ul>
                                 <li dir="ltr">
-                                    <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">How should the costs of the policy collaboration be distributed among developing and developed countries?</b></p>
+                                    <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">How should the costs of the policy collaboration be distributed among developing and developed countries?</span></p>
                                 </li>
                                 <li dir="ltr">
-                                    <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">To what extent is the emission trading system working? What fosters and what hinders this finance mechanism? </b></p>
+                                    <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">To what extent is the emission trading system working? What fosters and what hinders this finance mechanism? </span></p>
                                 </li>
                                 <li dir="ltr">
-                                    <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">Do we need to create a new international institution (analogous to the IMF) to deal with climate change, or can existing institutions handle the task?</b></p>
+                                    <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">Do we need to create a new international institution (analogous to the IMF) to deal with climate change, or can existing institutions handle the task?</span></p>
                                 </li>
                                 <li dir="ltr">
-                                    <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">How does a desirable institutional architecture for climate negotiations look like?</b></p>
+                                    <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">How does a desirable institutional architecture for climate negotiations look like?</span></p>
                                 </li>
                                 <li dir="ltr">
-                                    <p dir="ltr"><b id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">To what extent may the current political turbulence in major benefactors affect the global atmosphere in tackling climate change? What is the role of international technology cooperation and adaptation measures to climate change?</b></p>
+                                    <p dir="ltr"><span id="docs-internal-guid-701c63ca-47b8-38ed-4c0e-d75d41655882">To what extent may the current political turbulence in major benefactors affect the global atmosphere in tackling climate change? What is the role of international technology cooperation and adaptation measures to climate change?</span></p>
                                 </li>
                             </ul>
 
