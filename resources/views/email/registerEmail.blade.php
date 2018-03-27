daa5c1192f5f87d4c8d94768e5ba20279ab6e33a
Hin <hunguet1471994@gmail.com>
2018-03-27 16:39:54 +0700

    UPDATE

--- resources/views/email/registerEmail.blade.php
+++ resources/views/email/registerEmail.blade.php
@@ -36,11 +36,11 @@
 <span id="docs-internal-guid-bce07a37-56fd-a84c-d641-59fb31f6947b">Hanoi Forum Secretariat</span><br>
 <span id="docs-internal-guid-bce07a37-56fd-a84c-d641-59fb31f6947b">R1006, Administration Building</span><br>
 <span id="docs-internal-guid-bce07a37-56fd-a84c-d641-59fb31f6947b">Vietnam National University, Hanoi</span><br>
-<a href="#" style="color: inherit;text-decoration: none;">144 Xuan Thuy road, Cau Giay district, Hanoi, Vietnam</a><br>
+<a href="#" style="color: inherit;text-decoration: none;">144 Xuan Thuy road, Cau Giay district</a><br>
+<span><a href="#" style="color: inherit;text-decoration: none;">Hanoi, Vietnam</a></span><br>
 <span >Tel: (84) 24 37547670 - Ext: 723</span><br>
-<span class="contact">W:https://hanoiforum.vnu.edu.vn</span>
+<span class="contact">E: hanoiforum@vnu.edu.vn</span><br>
+<span class="contact">W: https://hanoiforum.vnu.edu.vn</span>
 <br />
 <br />
 <p dir="ltr"><em>If you didn&rsquo;t </em><em>create an account on Hanoi Forum website, just ignore this email. </em></p>
-
-&nbsp;
--- resources/views/frontend/layouts/banner.blade.php
+++ resources/views/frontend/layouts/banner.blade.php
@@ -37,61 +37,51 @@
                         {{--</div>--}}
                     {{--</li>--}}
 
-                    <li class="seq-step1" id="step1" style="background:url(/frontend/banner/slide.jpg);background-repeat: no-repeat;background-size: cover;">
+                    <li class="seq-step1" id="step1" style="background:url(/frontend/banner/slide1.jpg);background-repeat: no-repeat;background-size: cover;">
                         <div class="bg-blend"></div>
-                        <div class="seq-content" >
+                        {{--<div class="seq-content" >--}}
 
                             {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
                             {{--<div class="seq-meta" data-seq="">--}}
                                 {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
                             {{--</div>--}}
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        </div>
+                        {{--</div>--}}
                     </li>
-                    <li class="seq-step2" id="step2" style="background:url(/frontend/banner/2.jpg);background-repeat: no-repeat;background-size: cover;">
+                    <li class="seq-step2" id="step2" style="background:url(/frontend/banner/slide2.jpg);background-repeat: no-repeat;background-size: cover;">
                         <div class="bg-blend"></div>
-                        <div class="seq-content" >
+                        {{--<div class="seq-content" >--}}
 
                             {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
                             {{--<div class="seq-meta" data-seq="">--}}
                             {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
                             {{--</div>--}}
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        </div>
+                        {{--</div>--}}
                     </li>
-                    <li class="seq-step3" id="step3" style="background:url(/frontend/banner/3.jpg);background-repeat: no-repeat;background-size: cover;">
+                    <li class="seq-step3" id="step3" style="background:url(/frontend/banner/slide3.jpg);background-repeat: no-repeat;background-size: cover;">
                         <div class="bg-blend"></div>
-                        <div class="seq-content" >
+                        {{--<div class="seq-content" >--}}
 
                             {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
                             {{--<div class="seq-meta" data-seq="">--}}
                             {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
                             {{--</div>--}}
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        </div>
+                        {{--</div>--}}
                     </li>
-                    <li class="seq-step4" id="step4" style="background:url(/frontend/banner/4.jpg);background-repeat: no-repeat;background-size: cover;">
+                    <li class="seq-step4" id="step4" style="background:url(/frontend/banner/slide4.jpg);background-repeat: no-repeat;background-size: cover;">
                         <div class="bg-blend"></div>
-                        <div class="seq-content" >
+                        {{--<div class="seq-content" >--}}
 
                             {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
                             {{--<div class="seq-meta" data-seq="">--}}
                             {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
                             {{--</div>--}}
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        </div>
+                        {{--</div>--}}
                     </li>
-                    <li class="seq-step5" id="step5" style="background:url(/frontend/banner/5.jpg);background-repeat: no-repeat;background-size: cover;">
-                        <div class="bg-blend"></div>
-                        <div class="seq-content" >
 
-                            {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
-                            {{--<div class="seq-meta" data-seq="">--}}
-                            {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
-                            {{--</div>--}}
-                            {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        </div>
-                    </li>
                 </ul>
                 <div class="sec-navigate-wrap pos-y_center">
                     <button type="button" class="seq-next"></button>
