fa997dd05e63735133b0a00d0732f34d9397277f
Hin <hunguet1471994@gmail.com>
2018-03-27 14:34:35 +0700

    UPDATE ABOUT HANOI FORUM

--- resources/views/frontend/hanoiForum.blade.php
+++ resources/views/frontend/hanoiForum.blade.php
@@ -15,11 +15,20 @@
                     <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
 
                         <div class="">
-                            <p>Hanoi Forum is an international academic conference co-hosted by Vietnam National University, Hanoi (VNU) and Korea Foundation for Advanced Studies (KFAS).</p>
+                            <p>Hanoi Forum is an international academic conference co-hosted by Vietnam National
+                                University, Hanoi (VNU) and Korea Foundation for Advanced Studies (KFAS).</p>
 
-                            <p>The general theme of Hanoi Forum is &ldquo;Towards Sustainable Development&rdquo;. Hanoi Forum aims at promoting the study of sustainability science around the world. It encourages interdisciplinary research that push our knowledge limits in understanding the interactions between human and environment, socio-economic growth and natural conservation in order to contribute to the development of a prosperous, safe, and resilient society.</p>
+                            <p>The general theme of Hanoi Forum is &ldquo;Towards Sustainable Development&rdquo;. Hanoi
+                                Forum aims at promoting the goal of inclusive and sustainable development for all. It
+                                encourages interdisciplinary research that push our knowledge limits in understanding
+                                socio-economic growth and conservation, interactions between human and environment,
+                                prosperity and equality.</p>
 
-                            <p>Hanoi Forum believes the development of human being depends on our ability to deepening understanding about growth and creative solutions for complex challenges that threaten the future of humanity and the integrity of the life support systems of the planet, such as climate change, biodiversity loss, pollution, land and water degradation.</p>
+                            <p>Hanoi Forum believes the development of human being depends on our ability to deepening
+                                understanding about growth and creative solutions for complex challenges that threaten
+                                the future of humanity and the integrity of the life support systems of the planet, such
+                                as climate change, biodiversity loss, pollution, land and water degradation.</p>
+
 
                         </div>
 
