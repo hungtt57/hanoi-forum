187d94fd88fdd6be3fb9184bd50a705e407a03c9
Hin <hunguet1471994@gmail.com>
2018-03-27 16:53:23 +0700

    UPDATE

--- resources/views/frontend/layouts/banner.blade.php
+++ resources/views/frontend/layouts/banner.blade.php
@@ -36,7 +36,17 @@
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
                         {{--</div>--}}
                     {{--</li>--}}
+                    <li class="seq-step0" id="step0" style="background:url(/frontend/banner/slide.jpg);background-repeat: no-repeat;background-size: cover;">
+                        <div class="bg-blend"></div>
+                        {{--<div class="seq-content" >--}}
 
+                        {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
+                        {{--<div class="seq-meta" data-seq="">--}}
+                        {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
+                        {{--</div>--}}
+                        {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
+                        {{--</div>--}}
+                    </li>
                     <li class="seq-step1" id="step1" style="background:url(/frontend/banner/slide1.jpg);background-repeat: no-repeat;background-size: cover;">
                         <div class="bg-blend"></div>
                         {{--<div class="seq-content" >--}}
