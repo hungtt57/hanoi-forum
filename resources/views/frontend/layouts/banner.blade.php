c702a0fbb8ff6d8fa9aa998fbfd5bcf977425e66
Hin <hunguet1471994@gmail.com>
2018-04-03 17:12:10 +0700

    UP

--- resources/views/frontend/layouts/banner.blade.php
+++ resources/views/frontend/layouts/banner.blade.php
@@ -37,17 +37,9 @@
                             {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
                         {{--</div>--}}
                     </li>
-                    <li class="seq-step3" id="step3" style="background:url(/frontend/banner/slide3.jpg);background-repeat: no-repeat;background-size: cover;background-position: center center;">
-                        <div class="bg-blend"></div>
-                        {{--<div class="seq-content" >--}}
-
-                            {{--<h2 class="seq-title font-2 tt_up" data-seq="">Where it all began</h2>--}}
-                            {{--<div class="seq-meta" data-seq="">--}}
-                            {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
-                            {{--</div>--}}
-                            {{--<a href="#" class="btn-link btn-more">READ MORE</a>--}}
-                        {{--</div>--}}
-                    </li>
+                    {{--<li class="seq-step3" id="step3" style="background:url(/frontend/banner/slide3.jpg);background-repeat: no-repeat;background-size: cover;background-position: center center;">--}}
+                        {{--<div class="bg-blend"></div>--}}
+                    {{--</li>--}}
                     <li class="seq-step4" id="step4" style="background:url(/frontend/banner/slide4.jpg);background-repeat: no-repeat;background-size: cover;background-position: center center;">
                         <div class="bg-blend"></div>
                         {{--<div class="seq-content" >--}}
