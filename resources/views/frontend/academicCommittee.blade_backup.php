2ff8ccc216211028fb52b1b43bd51d46ae770e1e
Hin <hunguet1471994@gmail.com>
2018-04-09 14:33:14 +0700

    up

---
+++ resources/views/frontend/academicCommittee.blade_backup.php
@@ -0,0 +1,128 @@
+@extends('frontend.layouts.master')
+@section('title','Home')
+@section('content')
+    <header class="sabbi-page-header page-header-lg">
+        <div class="page-header-content conternt-center">
+            <div class="header-title-block">
+                <h1 class="page-title">Academic Committee</h1>
+            </div>
+        </div>
+    </header>
+    <main class="sabbi-page-wrap">
+        <div class="container">
+            <section class="bio__holder">
+                <div class="row">
+                    <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
+                        <div>
+                            <p><strong>Mai Trong Nhuan, </strong>Professor</p>
+
+                            <p>Affiliation(s): &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+
+                            <ul>
+                                <li>Member of the National Steering Committee for International Integration of Education, Science, and Technology.</li>
+                                <li>Chairman of the VNU &ndash; Council for Education Quality Assurance.</li>
+                                <li>Former Director of VNU.</li>
+                            </ul>
+
+                            <p>Research areas: Earth science, Environment, Climate change, Sustainable development.</p>
+
+                            <p>&nbsp;</p>
+
+
+                            <p><strong>Nils Roar Salthun</strong><strong>, </strong>Emeritus Professor</p>
+
+                            <p>Affiliation(s): former Head of Department of Geosciences at the University of Oslo, Norway.</p>
+
+                            <p>Research areas: Development and applications of hydrological models, Water resources management, impacts of climate change impacts on water resources, Hydropower hydrology, Floods, design flood estimation, Environmental information systems.</p>
+
+                            <p>&nbsp;</p>
+
+
+                            <p><strong>Diana &Uuml;rge-Vorsatz, </strong>Professor</p>
+
+                            <p>Affiliation(s):</p>
+
+                            <ul>
+                                <li>Director, Center for Climate Change and Sustainable Energy Policy (3CSEP).</li>
+                                <li>Dept. of Environmental Sciences and Policy, Central European University (CEU).</li>
+                                <li>Vice Chair of Working Group III, the Intergovernmental Panel on Climate Change (IPCC).</li>
+                                <li>Leader, Scientific review of Sustainable Development Goal &ldquo;7&rdquo; in the new UN Development goals development process (Ensure access to affordable, reliable, sustainable, and modern energy for all&rdquo;), 2014.</li>
+                                <li>Member of the Scientific Committee of the high-level conference, &ldquo;Our Common Future under Climate Change&rdquo;, Paris, 2015.&nbsp;</li>
+                            </ul>
+
+                            <p>Research areas<strong>:</strong> Environmental and Energy Studies.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Cuong Le Van</strong><strong>, </strong>Emeritus Professor</p>
+
+                            <p>Affiliation(s):</p>
+
+                            <ul>
+                                <li>Paris School of Economics.</li>
+                                <li>Former Research Director, French National for Scientific Research (CNRS).</li>
+                            </ul>
+
+                            <p>Research areas: Quantitative Methods, Macroeconomics and asset accumulation, Macroeconomics dynamics, General equilibrium.</p>
+
+                            <p>&nbsp;</p>
+
+
+
+                            <p><strong>Vu Minh Giang, </strong>Professor</p>
+
+                            <p>Affiliation(s):</p>
+
+                            <ul>
+                                <li>Chairman of VNU - Council for Science and Training.</li>
+                                <li>Member of the National Council for Science and Technology.</li>
+                                <li>Former Deputy Director of VNU.</li>
+                            </ul>
+
+                            <p>Research areas:</p>
+
+                            <p>&nbsp;</p>
+
+
+
+                            <p><strong>Truong Quang Hoc, </strong>Professor</p>
+
+                            <p>Affiliation(s):</p>
+
+                            <ul>
+                                <li>Deputy Head of Vietnam Association for Conservation of Nature and Environment.</li>
+                                <li>Member of VNU &ndash; Centre Institute for Natural Resources&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; and Environmental Studies.</li>
+                            </ul>
+
+                            <p>Research areas: Science of Sustainability, Sustainable Development.</p>
+
+                            <p>&nbsp;</p>
+
+
+                            <p><strong>Hoang Van Thang, </strong>Professor</p>
+
+                            <p>Affiliation(s): Director, VNU &ndash; Centre Institute for Natural Resources and Environmental Studies.</p>
+
+                            <p>Research areas: Management and conservation of wetlands, Environmental planning, Biodiversity conservation, Sustainable development.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Phan Van Tan, </strong>Professor</p>
+
+                            <p>Affiliation(s): VNU University of Science.</p>
+
+                            <p>Research areas: Meteorology, Climate change.</p>
+
+                            <p>&nbsp;</p>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </section><!-- /.bio__holder -->
+
+        </div>
+
+    </main>
+@endsection
---
+++ resources/views/frontend/organizingCommittee_backup.blade.php
@@ -0,0 +1,126 @@
+@extends('frontend.layouts.master')
+@section('title','Home')
+@section('content')
+    <header class="sabbi-page-header page-header-lg">
+        <div class="page-header-content conternt-center">
+            <div class="header-title-block">
+                <h1 class="page-title">Organizing Committee</h1>
+            </div>
+        </div>
+    </header>
+    <main class="sabbi-page-wrap">
+        <div class="container">
+            <section class="bio__holder">
+                <div class="row">
+                    <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
+                        <div>
+                            <p align="center"><strong>Chairman</strong></p>
+                            
+                            <p><strong>Nguyen Kim Son</strong><strong>,</strong> <strong>Assoc. Professor</strong></p>
+
+                            <p>President of Vietnam National University, Hanoi.</p>
+
+
+
+                            <p align="center"><strong>Commitee members</strong></p>
+
+                            <p align="center">&nbsp;</p>
+
+                            <p><strong>Nguyen Thi Anh Thu</strong><strong>, Ph.D</strong></p>
+
+                            <p>Director of Cooperation and Development Department</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Mai Trong Nhuan, Professor</strong></p>
+
+                            <p>Chairman, Council for Education Quality Assurance</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Vu Minh Giang, Professor</strong></p>
+
+                            <p>Chairman, Council for Science and Training</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Phan Ngoc Minh, Professor</strong></p>
+
+                            <p>Vice Chair of&nbsp;Vietnam Academy of Science and Technology.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Do Canh Duong, Assoc. Professor</strong></p>
+
+                            <p>General Director, General Department of&nbsp;Geology and Minerals<br />
+                                Ministry of Natural Resources and Environment.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Pham Hoang Mai, Ph.D</strong></p>
+
+                            <p>Director, Department of Science, Education, Natural Resources and Environment Vietnam Ministry of Planning and Investment.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Kensuke Fukushi</strong><strong>,</strong> <strong>Professor</strong></p>
+
+                            <p>University of Tokyo.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Tran Quoc Binh, PhD</strong></p>
+
+                            <p>Chief Office of the President</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Vu Van Tich, Assoc. Professor</strong></p>
+
+                            <p>Director, Department of Science and Technology<br />
+                                Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Nguyen Hoang Oanh, PhD</strong></p>
+
+                            <p>Vice President, Vietnam Japan University</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Nguyen Thi Hong Minh, PhD</strong></p>
+
+                            <p>Dean of School of Interdisciplinary Studies</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Ms. Mary Lee</strong></p>
+
+                            <p>International Academic Division, Korea Foundation for Advanced Studies.</p>
+
+                            <p>&nbsp;</p>
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </section><!-- /.bio__holder -->
+
+        </div>
+
+    </main>
+@endsection
