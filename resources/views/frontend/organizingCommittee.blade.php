30fb47fcf2800c9bde2be75436ba8bca766612b5
Hin <hunguet1471994@gmail.com>
2018-04-09 14:59:08 +0700

    up

--- resources/views/frontend/organizingCommittee.blade.php
+++ resources/views/frontend/organizingCommittee.blade.php
@@ -14,13 +14,104 @@
                 <div class="row">
                     <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
                         <div>
-                            <br />
-                            <meta charset="utf-8" />
-                            <p dir="ltr"><b id="docs-internal-guid-79b16f49-46e0-b053-ac3f-4731e10ec0d0">Co-Chairs of Organizing Committee </b></p>
+                            <p align="center"><strong>Chairman</strong></p>
 
-                            <p dir="ltr"><span id="docs-internal-guid-79b16f49-46e0-b053-ac3f-4731e10ec0d0">Prof. Nguyen Kim Son, Vietnam National University, Hanoi </span></p>
+                            <p><strong>Nguyen Kim Son</strong><strong>,</strong> <strong>Assoc. Professor</strong></p>
 
-                            <p dir="ltr"><span id="docs-internal-guid-79b16f49-46e0-b053-ac3f-4731e10ec0d0">Dr. Park In Kook, Korea Foundation for Advanced Studies</span></p>
+                            <p>President of Vietnam National University, Hanoi.</p>
+
+
+
+                            <p align="center"><strong>Commitee members</strong></p>
+
+                            <p align="center">&nbsp;</p>
+
+                            <p><strong>Nguyen Thi Anh Thu</strong><strong>, Ph.D</strong></p>
+
+                            <p>Director of Cooperation and Development Department</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Mai Trong Nhuan, Professor</strong></p>
+
+                            <p>Chairman, Council for Education Quality Assurance</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Vu Minh Giang, Professor</strong></p>
+
+                            <p>Chairman, Council for Science and Training</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Phan Ngoc Minh, Professor</strong></p>
+
+                            <p>Vice Chair of&nbsp;Vietnam Academy of Science and Technology.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Do Canh Duong, Assoc. Professor</strong></p>
+
+                            <p>General Director, General Department of&nbsp;Geology and Minerals<br />
+                                Ministry of Natural Resources and Environment.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Pham Hoang Mai, Ph.D</strong></p>
+
+                            <p>Director, Department of Science, Education, Natural Resources and Environment Vietnam Ministry of Planning and Investment.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Kensuke Fukushi</strong><strong>,</strong> <strong>Professor</strong></p>
+
+                            <p>University of Tokyo.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Tran Quoc Binh, PhD</strong></p>
+
+                            <p>Chief Office of the President</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Vu Van Tich, Assoc. Professor</strong></p>
+
+                            <p>Director, Department of Science and Technology<br />
+                                Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Nguyen Hoang Oanh, PhD</strong></p>
+
+                            <p>Vice President, Vietnam Japan University</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Nguyen Thi Hong Minh, PhD</strong></p>
+
+                            <p>Dean of School of Interdisciplinary Studies</p>
+
+                            <p>Vietnam National University, Hanoi.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p><strong>Ms. Mary Lee</strong></p>
+
+                            <p>International Academic Division, Korea Foundation for Advanced Studies.</p>
+
+                            <p>&nbsp;</p>
+
 
 
                         </div>
