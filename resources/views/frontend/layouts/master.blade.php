6e12ce3e37f6516c18c79502872e46dd8b8ff0bd
Hin <hunguet1471994@gmail.com>
2018-04-17 16:50:53 +0700

    up

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -35,14 +35,12 @@
 
     <style>
         @media (min-width: 1025px) and (max-width: 1280px) {
-
             .img-logo {
                 width: 60%;
             }
 
             #menu-main-nav {
                 font-size: 9px !important;
-
             }
         }
 
@@ -206,88 +204,6 @@
                     </div>
                 </section>
             </div>
-            {{--<div class="col-sm-3">--}}
-            {{--<div class="footer-site-info">--}}
-            {{--<header>--}}
-
-            {{--<a class="footer-brand" href="http://vnu.edu.vn/home/"><img alt="" class="img-responsive"--}}
-            {{--style="max-width: 90px"--}}
-            {{--src="/frontend/assets/img/site-logo.png"></a>--}}
-
-            {{--</header>--}}
-            {{--<address>--}}
-            {{--<h3 class="entry-title">Phone and Email</h3>--}}
-
-            {{--</address>--}}
-            {{--<footer class="contact-info">--}}
-            {{--<h3 class="entry-title">Contact Information</h3>--}}
-            {{--<p class="__ci_num">Call: <span>+61(07)373 53921</span></p>--}}
-            {{--<p class="__ci_num">Email: <span>rushmore@mail.com</span></p>--}}
-            {{--</footer>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-
-            {{--<div class="col-sm-9">--}}
-            {{--<div class="row">--}}
-            {{--<div class="footer-site-info col-xs-12">--}}
-            {{--<h3 class="entry-title">Co-hosts</h3>--}}
-            {{--<div class="row pull-left hidden-xs">--}}
-            {{--<a href="https://www.vnu.edu.vn/home/" target="_blank">--}}
-            {{--<img style="max-width: 90px" src="/frontend/assets/img/vnu.png" alt="">--}}
-            {{--</a>--}}
-            {{--<a href="http://kfas.or.kr/" target="_blank">--}}
-            {{--<img style="max-width: 360px;margin-left:10px" src="/frontend/assets/img/kfas_logo.jpg"--}}
-            {{--alt="">--}}
-            {{--</a>--}}
-
-            {{--</div>--}}
-            {{--<div class="row  hidden-sm hidden-md hidden-lg " style="text-align: center">--}}
-            {{--<a href="https://www.vnu.edu.vn/home/" target="_blank">--}}
-            {{--<img style="max-width: 70px" src="/frontend/assets/img/vnu.png" alt="">--}}
-            {{--</a>--}}
-
-            {{--<a href="http://kfas.or.kr/" target="_blank">--}}
-            {{--<img style="max-width: 140px;margin-left:20px" src="/frontend/assets/img/kfas_logo.jpg"--}}
-            {{--alt=""></a>--}}
-
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="row " id="partner">--}}
-            {{--<div class="footer-site-info col-xs-12">--}}
-            {{--<h3 class="entry-title">Partners</h3>--}}
-            {{--<div class="row pull-left hidden-xs" id="partner">--}}
-            {{--<a href="https://www.most.gov.vn/vn/Pages/Trangchu.aspx" target="_blank">--}}
-            {{--<img style="max-width: 90px" src="/frontend/bkhvcn.png" alt="">--}}
-            {{--</a>--}}
-            {{--<a href="http://www.monre.gov.vn" target="_blank">--}}
-            {{--<img style="max-width: 90px" src="/frontend/btnvmt.png" alt=""></a>--}}
-            {{--<a href="http://hanoi.gov.vn/home" target="_blank">--}}
-            {{--<img style="max-width: 90px" src="/frontend/hanoi.png" alt=""></a>--}}
-            {{--<a href="http://www.vast.ac.vn/en/" target="_blank">--}}
-            {{--<img style="max-width: 200px" src="/frontend/vastlogo.png" alt="">--}}
-            {{--</a>--}}
-
-            {{--</div>--}}
-            {{--<div class="row  hidden-sm hidden-md hidden-lg " style="text-align: center" id="partner">--}}
-            {{--<a href="https://www.most.gov.vn/vn/Pages/Trangchu.aspx" target="_blank" class="partner-footer">--}}
-            {{--<img style="max-width: 70px" src="/frontend/bkhvcn.png" alt="">--}}
-            {{--</a>--}}
-
-            {{--<a href="http://www.monre.gov.vn" target="_blank"   class="partner-footer">--}}
-            {{--<img style="max-width: 70px" src="/frontend/btnvmt.png" alt=""></a>--}}
-            {{--<a href="http://hanoi.gov.vn/home" target="_blank"   class="partner-footer">--}}
-            {{--<img style="max-width: 70px" src="/frontend/hanoi.png" alt=""></a>--}}
-            {{--<a href="http://www.vast.ac.vn/en/" target="_blank"  class="partner-footer">--}}
-            {{--<img style="max-width: 120px" src="/frontend/vastlogo.png" alt="">--}}
-            {{--</a>--}}
-
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div>--}}
-
-
-            {{--</div>--}}
         </div>
         <div id="site-footer-bar" class="footer-bar mt_30">
             <div class="container">
