f2f4b8855e2b79f62cbc71603ef4bfaae3ee2c59
Hin <hunguet1471994@gmail.com>
2018-07-05 09:24:15 +0700

    up

--- resources/views/frontend/hanoiForum2018.blade.php
+++ resources/views/frontend/hanoiForum2018.blade.php
@@ -44,9 +44,9 @@
 
                         <p><strong>3)Climate change response: </strong>Climate change adaptation; Climate change mitigation; Climate resilient economic and livelihood development; Resilient infrastructure development in the context of climate change; Disaster and climate-resilient communities; Resilient cities; Nature and ecosystem adaptation; Innovative measures for greenhouse gas mitigation.</p>
 
-                        <p><strong>4) Policy and governance of climate change response and sustainability: </strong>Policies in response to climate change; International politics of climate change; International agreements on climate change policy; Strategies for sustainability; Human adaptive strategies.</p>
+                        <p><strong>4)Policy and governance of climate change response and sustainability: </strong>Policies in response to climate change; International politics of climate change; International agreements on climate change policy; Strategies for sustainability; Human adaptive strategies.</p>
 
-                        <p><strong>5) Science, technology and education for climate change response and sustainability: </strong>Education and awareness for management of global climate change; Higher education on climate change response; Alternative and renewable energy sources: technologies, policies, and strategies; Technologies of mitigation: carbon dioxide sequestration, solar shades, and other processes; Low carbon and green technology for climate change response and sustainable development.</p>
+                        <p><strong>5)Science, technology and education for climate change response and sustainability: </strong>Education and awareness for management of global climate change; Higher education on climate change response; Alternative and renewable energy sources: technologies, policies, and strategies; Technologies of mitigation: carbon dioxide sequestration, solar shades, and other processes; Low carbon and green technology for climate change response and sustainable development.</p>
 
                     </div>
 
