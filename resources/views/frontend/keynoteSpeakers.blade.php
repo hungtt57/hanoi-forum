be4e90d08c312b3b52f8ba5c1bcdb83828b48469
Hin <hunguet1471994@gmail.com>
2018-08-14 15:22:02 +0700

    up

--- resources/views/frontend/keynoteSpeakers.blade.php
+++ resources/views/frontend/keynoteSpeakers.blade.php
@@ -21,10 +21,12 @@
             <style>
 
                 .image-human img {
-                    width: 150px;
+                    width: 200px;
+                    margin-left: 10px;
 
                 }
                 .content-human {
+                    text-align: justify;
                     margin-bottom: 10px;
                     border-bottom: 1px solid black;
                     padding-bottom: 10px;
@@ -73,6 +75,26 @@
                                 <div class="clearfix"></div>
                             </div>
 
+                            <div class="col-md-12 content-human">
+
+                                <div class="image-human">
+                                    <img class="" src="/frontend/keynote/groff.jpg" alt="">
+                                </div>
+                                <p><strong>STEPHEN P. GROFF</strong></p>
+                                <p>Stephen P. Groff is Vice-President (Operations 2) of the Asian Development Bank (ADB). He assumed office in October 2011.</p>
+                                <p>Mr. Groff is responsible for the full range of ADB's operations in East Asia, Southeast Asia, and the Pacific. His mandate includes establishing strategic and operational priorities in his areas of responsibility, producing investment and technical assistance operations amounting to approximately $5 billion annually, managing an existing portfolio of about $31 billion, and leading about 650 staff.
+                                </p>
+                                <p> In addition, Mr. Groff supports ADB's President in managing ADB's overall operations, represents ADB in high-level multilateral fora, and contributes to managing its relationships with its 67 member country shareholders, other multilateral financial institutions, and key government, private sector, and civil society partners.
+
+                                </p>
+                                <p>Prior to joining ADB, Mr. Groff was Deputy Director for Development Cooperation at the Paris-based Organisation for Economic Co-operation and Development (OECD) where he led OECD's work on a wide range of development-related economic and political issues. He also served as OECD's envoy to the G20 Working Group on Development and was a member of the World Economic Forum's Global Agenda Council. Prior to this he was the Deputy Vice-President for Operations at the Washington-based Millennium Challenge Corporation (MCC), where he helped set up the agency and led MCC programs while advising the CEO on development issues, strategy, and policy. Prior to MCC, Mr. Groff held several staff positions at the ADB.  Before this, Mr. Groff was the deputy director and chief economist on a large U.S. Agency for International Development project designed to encourage private sector development in the southern Philippines, a Program Director for the U.S. Refugee Program, and a U.S. Peace Corps Volunteer.
+                                </p>
+                                <p>Mr. Groff has worked across Asia, Africa, and Latin America and writes regularly on development issues. He also serves on a number of advisory boards for development-related organizations.
+                                </p>
+                                <p>Mr. Groff holds a Master's degree in Public Administration from Harvard University and a Bachelor of Science degree in Environmental Biology from Yale University.
+                                </p>
+                                <div class="clearfix"></div>
+                            </div>
 
                         </div>
                     </div>
