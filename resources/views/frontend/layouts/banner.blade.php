e02f99e73a9985fb1ab7de831fc2c523daca0583
Hin <hunguet1471994@gmail.com>
2018-08-14 15:56:19 +0700

    up

--- resources/views/frontend/layouts/banner.blade.php
+++ resources/views/frontend/layouts/banner.blade.php
@@ -14,24 +14,24 @@ $agent = new \Jenssegers\Agent\Agent();
             <div class="seq seq--kawsa" id="sequence">
                 <div class="seq-screen">
                     <ul class="seq-canvas">
-                        @php $posts = App\Models\Post::where('status',1)->orderBy('created_at','desc')->limit(2)->get(); @endphp
-                        @foreach($banners as $banner)
+                        @php $posts = App\Models\Post::where('status',1)->orderBy('created_at','desc')->get()->toArray(); @endphp
+                        @foreach($banners as $index => $banner)
 
-                            @if(!$agent->isMobile())
-                            <li class="seq-step{{$banner->id}}" id="step{{$banner->id}}"
-                                style="background:url({{$banner->image}});background-repeat: no-repeat;background-size: cover;background-position: center center;">
-                                @else
-                                @php $name = basename($banner->image); @endphp
+                                @if(!$agent->isMobile())
                                 <li class="seq-step{{$banner->id}}" id="step{{$banner->id}}"
-                                    style="background:url({{'/files/blur-'.$name}});background-repeat: no-repeat;background-size: cover;background-position: center center;">
-                                    @endif
+                                    style="background:url({{$banner->image}});background-repeat: no-repeat;background-size: cover;background-position: center center;">
+                                    @else
+                                    @php $name = basename($banner->image); @endphp
+                                    <li class="seq-step{{$banner->id}}" id="step{{$banner->id}}"
+                                        style="background:url({{'/files/blur-'.$name}});background-repeat: no-repeat;background-size: cover;background-position: center center;">
+                                        @endif
                                 <div class="bg-blend"></div>
-                                @if($loop->index == 0)
-                                    @if($posts)
-                                        @if($posts->first())
+                                {{--@if($loop->index == 0)--}}
+                                    @if(count($posts))
+                                        @if(isset($posts[$index]))
                                         <div class="seq-content" >
-                                            <a href="{{url('post').'/'.str_slug($posts->first()->title).'-'.$posts->first()->id}}" style="border-bottom: none !important;" class="btn-link btn-more">
-                                            <h3 style="font-size: 25px;border-bottom: none !important;" class="seq-title font-2 tt_up" data-seq="">{{ $posts->first()->title }}</h3>
+                                            <a href="{{url('post').'/'.str_slug($posts[$index]['title']).'-'.$posts[$index]['id']}}" style="border-bottom: none !important;" class="btn-link btn-more">
+                                            <h3 style="font-size: 25px;border-bottom: none !important;" class="seq-title font-2 tt_up" data-seq="">{{ $posts[$index]['title']}}</h3>
                                             {{--<div class="seq-meta" data-seq="">--}}
                                                 {{--<p class="seq-meta-text">Congratulations Dr Rushmore : his research to develop a new detection strategy for cancer has received a new National Health and Medical Research Council </p>--}}
                                             {{--</div>--}}
@@ -40,24 +40,24 @@ $agent = new \Jenssegers\Agent\Agent();
                                         </div>
                                             @endif
                                         @endif
-                                    @endif
+                                    {{--@endif--}}
 
-                                @if($loop->index == 1)
-                                    @if($posts)
-                                        @if($posts[1])
+                                {{--@if($loop->index == 1)--}}
+                                    {{--@if($posts)--}}
+                                        {{--@if($posts[1])--}}
 
-                                    <div class="seq-content" >
-                                        <a href="{{url('post').'/'.str_slug($posts[1]->title).'-'.$posts[1]->id}}"  style="border-bottom: none !important;"  class="btn-link btn-more">
-                                        <h3 style="font-size: 25px;border-bottom: none !important;" class="seq-title font-2 tt_up" data-seq="">Announcing Hanoi Forum 2018</h3>
-                                        <div class="seq-meta" data-seq="">
+                                    {{--<div class="seq-content" >--}}
+                                        {{--<a href="{{url('post').'/'.str_slug($posts[1]->title).'-'.$posts[1]->id}}"  style="border-bottom: none !important;"  class="btn-link btn-more">--}}
+                                        {{--<h3 style="font-size: 25px;border-bottom: none !important;" class="seq-title font-2 tt_up" data-seq="">Announcing Hanoi Forum 2018</h3>--}}
+                                        {{--<div class="seq-meta" data-seq="">--}}
                                             {{--<p class="seq-meta-text">HANOI, Vietnam – Vietnam National University, Hanoi (VNU) and Korea Foundation for Advanced Studies (KFAS) will co-host Hanoi Forum 2018, an international academic conference on climate change response and sustainable development, on November 8- 10, 2018 in Hanoi, Vietnam.</p>--}}
-                                        </div>
-                                        <p  class="btn-link btn-more" >READ MORE</p>
-                                        </a>
-                                    </div>
-                                            @endif
-                                            @endif
-                                @endif
+                                        {{--</div>--}}
+                                        {{--<p  class="btn-link btn-more" >READ MORE</p>--}}
+                                        {{--</a>--}}
+                                    {{--</div>--}}
+                                            {{--@endif--}}
+                                            {{--@endif--}}
+                                {{--@endif--}}
 
                             </li>
                         @endforeach
