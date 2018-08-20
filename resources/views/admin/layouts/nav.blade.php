0b4cb09f324a2666ac14fd0c9f27f7d370a07c11
Hin <hunguet1471994@gmail.com>
2018-08-20 14:01:40 +0700

    UPDATE

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -132,7 +132,7 @@
                             href="{{url('admin/partner/edit')}}"><i class="fa   fa-user-circle"></i>
                         <span>Manage your profile</span></a></li>
                 @if(auth('backend')->user()->apply)
-                    @if(auth('backend')->user()->created_at < Carbon\Carbon::createFromDate(2018,8,1))
+                    @if(auth('backend')->user()->created_at < Carbon\Carbon::createFromDate(2018,8,31))
                     <li class="{{ (Request::is('admin/submit')) ? 'active' : '' }}"><a
                                 href="{{url('admin/submit')}}"><i class="fa   fa-cloud-upload"></i>
                             <span>Submit your abstract</span></a></li>
