f5104475ced2fd0dd2b57f6df9a43834474e0c5e
Hưng <hunguet1471994@gmail.com>
2018-09-14 21:04:13 +0700

    up

--- resources/views/frontend/hanoiForum.blade.php
+++ resources/views/frontend/hanoiForum.blade.php
@@ -23,19 +23,10 @@
                     <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
 
                         <div class="content-page">
-                            <p>Hanoi Forum is an international academic conference co-hosted by Vietnam National
-                                University, Hanoi (VNU) and Korea Foundation for Advanced Studies (KFAS).</p>
-
-                            <p>The general theme of Hanoi Forum is &ldquo;Towards Sustainable Development&rdquo;. Hanoi
-                                Forum aims at promoting the goal of inclusive and sustainable development for all. It
-                                encourages interdisciplinary research that push our knowledge limits in understanding
-                                socio-economic growth and conservation, interactions between human and environment,
-                                prosperity and equality.</p>
-
-                            <p>Hanoi Forum believes the development of human being depends on our ability to deepening
-                                understanding about growth and creative solutions for complex challenges that threaten
-                                the future of humanity and the integrity of the life support systems of the planet, such
-                                as climate change, biodiversity loss, pollution, land and water degradation.</p>
+                            @php $article = \App\Models\Article::where('title','hanoi-forum')->first(); @endphp
+                            @if($article)
+                                {!! $article->content !!}
+                            @endif
 
 
                         </div>
--- resources/views/frontend/hanoiForum2018.blade.php
+++ resources/views/frontend/hanoiForum2018.blade.php
@@ -22,31 +22,35 @@
                 <div class="row">
                     <div class="col-md-8 col-sm-5 col-md-offset-2 col-xs-12">
                         <div class="content-page">
-                        <p>Hanoi Forum 2018 focuses on the study of human ecology, climate change response and governance of natural resources, thus titled &ldquo;Climate Change Response for Sustainability and Security&rdquo;. The Forum facilitates inter-national academic exchange and discussion in order to increase the effectiveness of climate change response and contribute to realize the goal of sustainable development at the global, regional and national levels. It aims to bring together scientists, policy makers, managers, and entrepreneurs from all over the world to engage in interdisciplinary conversations of natural resources management, energy, infrastructure, economics, and science and technology, to name a few, in order to:</p>
+                            @php $article = \App\Models\Article::where('title','hanoi-forum-2018')->first(); @endphp
+                            @if($article)
+                                {!! $article->content !!}
+                            @endif
+                        {{--<p>Hanoi Forum 2018 focuses on the study of human ecology, climate change response and governance of natural resources, thus titled &ldquo;Climate Change Response for Sustainability and Security&rdquo;. The Forum facilitates inter-national academic exchange and discussion in order to increase the effectiveness of climate change response and contribute to realize the goal of sustainable development at the global, regional and national levels. It aims to bring together scientists, policy makers, managers, and entrepreneurs from all over the world to engage in interdisciplinary conversations of natural resources management, energy, infrastructure, economics, and science and technology, to name a few, in order to:</p>--}}
 
-                        <p>- Identify and analyze evidence, best practices, successful factors and lessons learned from climate change response for sustainable development;</p>
+                        {{--<p>- Identify and analyze evidence, best practices, successful factors and lessons learned from climate change response for sustainable development;</p>--}}
 
-                        <p>- Support governments, private sector and social organizations to build a more active response strategies and policy to climate change;</p>
+                        {{--<p>- Support governments, private sector and social organizations to build a more active response strategies and policy to climate change;</p>--}}
 
-                        <p>- Contribute cutting-edge solutions to build a harmonious, low carbon and resilient society;</p>
+                        {{--<p>- Contribute cutting-edge solutions to build a harmonious, low carbon and resilient society;</p>--}}
 
-                        <p>- Develop education and research framework on climate change response; and</p>
+                        {{--<p>- Develop education and research framework on climate change response; and</p>--}}
 
-                        <p>- Foster collaboration for a better climate change response at international and regional levels.</p>
+                        {{--<p>- Foster collaboration for a better climate change response at international and regional levels.</p>--}}
 
-                        <h2>FOCUS</h2>
+                        {{--<h2>FOCUS</h2>--}}
 
-                        <p>Areas of interests include, but not limited to:</p>
+                        {{--<p>Areas of interests include, but not limited to:</p>--}}
 
-                        <p><strong>1) Climate change evidence and security: </strong>Sea level change; Floods, drought, forest fires, hurricanes, and other sporadic events; Disasters; Climate extreme; Climate change impact and vulnerability; Climate change resilience and adaptation; Riverine ecosystem impacts;&nbsp;&nbsp;&nbsp; Mountain ecosystem impacts; Coastal ecosystem impacts;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Marine ecosystem impacts; Impacts on humans: water supply, desertification; Climate measurement processes, methodologies, and technologies; Paleoclimatology;&nbsp;&nbsp;&nbsp;&nbsp; Environmental change; Energy sustainability;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Environment sustainability; Ecosystem sustainability.</p>
+                        {{--<p><strong>1) Climate change evidence and security: </strong>Sea level change; Floods, drought, forest fires, hurricanes, and other sporadic events; Disasters; Climate extreme; Climate change impact and vulnerability; Climate change resilience and adaptation; Riverine ecosystem impacts;&nbsp;&nbsp;&nbsp; Mountain ecosystem impacts; Coastal ecosystem impacts;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Marine ecosystem impacts; Impacts on humans: water supply, desertification; Climate measurement processes, methodologies, and technologies; Paleoclimatology;&nbsp;&nbsp;&nbsp;&nbsp; Environmental change; Energy sustainability;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Environment sustainability; Ecosystem sustainability.</p>--}}
 
-                        <p><strong>2)</strong> <strong>Human</strong><strong> impacts on climate change: </strong>Land-use change; Natural resource exploitations; Greenhouse gas emissions; Environment pollution.</p>
+                        {{--<p><strong>2)</strong> <strong>Human</strong><strong> impacts on climate change: </strong>Land-use change; Natural resource exploitations; Greenhouse gas emissions; Environment pollution.</p>--}}
 
-                        <p><strong>3)Climate change response: </strong>Climate change adaptation; Climate change mitigation; Climate resilient economic and livelihood development; Resilient infrastructure development in the context of climate change; Disaster and climate-resilient communities; Resilient cities; Nature and ecosystem adaptation; Innovative measures for greenhouse gas mitigation.</p>
+                        {{--<p><strong>3)Climate change response: </strong>Climate change adaptation; Climate change mitigation; Climate resilient economic and livelihood development; Resilient infrastructure development in the context of climate change; Disaster and climate-resilient communities; Resilient cities; Nature and ecosystem adaptation; Innovative measures for greenhouse gas mitigation.</p>--}}
 
-                        <p><strong>4)Policy and governance of climate change response and sustainability: </strong>Policies in response to climate change; International politics of climate change; International agreements on climate change policy; Strategies for sustainability; Human adaptive strategies.</p>
+                        {{--<p><strong>4)Policy and governance of climate change response and sustainability: </strong>Policies in response to climate change; International politics of climate change; International agreements on climate change policy; Strategies for sustainability; Human adaptive strategies.</p>--}}
 
-                        <p><strong>5)Science, technology and education for climate change response and sustainability: </strong>Education and awareness for management of global climate change; Higher education on climate change response; Alternative and renewable energy sources: technologies, policies, and strategies; Technologies of mitigation: carbon dioxide sequestration, solar shades, and other processes; Low carbon and green technology for climate change response and sustainable development.</p>
+                        {{--<p><strong>5)Science, technology and education for climate change response and sustainability: </strong>Education and awareness for management of global climate change; Higher education on climate change response; Alternative and renewable energy sources: technologies, policies, and strategies; Technologies of mitigation: carbon dioxide sequestration, solar shades, and other processes; Low carbon and green technology for climate change response and sustainable development.</p>--}}
 
                     </div>
 
