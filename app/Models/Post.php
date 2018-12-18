c86af42e89bfd1f436d2dd130f41640d09636b89
Hưng <hunguet1471994@gmail.com>
2018-12-18 09:34:27 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ class Post extends Model
 {
     public $fillable = ['content','content_en', 'title', 'user_id', 'status','meta_description','meta_title','meta_keyword','image'];
     public $table = 'posts';
+    
+    public functio getTitleAttribute() {
+     
+                            $raw_locale = \Session::get('locale');
+                 
+                        if($raw_locale != null and $raw_locale == 'vn') {
+                            return $this->attributes['title'];
+                        }
+        return $this->attributes['meta_title'];
+    
+    }
 }
